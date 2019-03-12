c40f2ea68550d181235fdf36ffd4178a38b62304
tyarn <tyarn@cs.roanoke.edu>
2019-03-12 16:13:33 -0400

    Researched and put some stuff into DefaultSettings DAO table, know how to create database entries and shuffle things in database around better

--- app/src/main/java/DefaultSettingsDao.java
+++ app/src/main/java/DefaultSettingsDao.java
@@ -1,6 +1,10 @@
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
 
 @Dao
 public interface DefaultSettingsDao {
-    //put queries following the guide here
+    //Inserts a defaultSettings row into the table.
+    @Insert
+    public void createDefaultSettings(DefaultSettings defaultSettings);
 }
