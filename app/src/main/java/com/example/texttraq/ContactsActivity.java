b2a06d28fc455031118d9a4e8c6b9ab2e0aa81c2
ajupright <ajupright@mail.roanoke.edu>
2019-03-12 16:14:54 -0400

    The "contacts" in main hopefully sends the user to the contacts screen

--- app/src/main/java/com/example/texttraq/ContactsActivity.java
+++ app/src/main/java/com/example/texttraq/ContactsActivity.java
@@ -1,7 +1,9 @@
 package com.example.texttraq;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ContactsActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/texttraq/MainActivity.java
+++ app/src/main/java/com/example/texttraq/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.texttraq;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }//this is a comment
+
+    Intent intent = new Intent(this, activity_contacts.class);
+
+    public void goToContacts(View view) {
+        startActivity(intent);
+    }
 }
