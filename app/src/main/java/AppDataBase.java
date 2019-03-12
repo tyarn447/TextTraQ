4242b61c450bec95fa8415c181afc41215012dd2
tyarn447 <tyarn447@gmail.com>
2019-03-12 19:56:09 -0400

    Created function to check if it is the first run of the app, if it is we will put in default values for database, havent figured that part yet still need to figure it out

--- app/src/main/java/AppDataBase.java
+++ app/src/main/java/AppDataBase.java
@@ -1,7 +1,8 @@
 import androidx.room.Database;
+import androidx.room.RoomDatabase;
 
 @Database(entities = {DefaultSettings.class,ContactTable.class, StateTable.class}, version = 1)
-public class AppDataBase extends RoomDatabase{
+public abstract class AppDataBase extends RoomDatabase {
     public abstract DefaultSettingsDao defaultDao();
     public abstract ContactTableDao contactDao();
     public abstract StateTableDao stateDao();
--- app/src/main/java/com/example/texttraq/MainActivity.java
+++ app/src/main/java/com/example/texttraq/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.texttraq;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.room.Room;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +14,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }//this is a comment
+        checkFirstRun();
+    }
 
     Intent intent = new Intent(this, activity_contacts.class);
 
     public void goToContacts(View view) {
         startActivity(intent);
     }
+
+
+    //PRE:Object exists
+    //POST:checks if this is the first run of the app, if it is puts default values into database
+    //else does nothing
+    public void checkFirstRun(){
+        boolean notFirstRun = false;
+
+        SharedPreferences settings = getSharedPreferences("PREFS_NAME",0);
+        notFirstRun = settings.getBoolean("FIRST_RUN",false);
+        if(!notFirstRun){
+            //ASSERT:This is the first time we are running the app
+            settings = getSharedPreferences("PREFS_NAME",0);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("FIRST_RUN",true);
+            editor.commit();
+            //Now put default values in
+
+            //fairly sure this is incorrect
+            AppDataBase db = Room.databaseBuilder(getApplicationContext(), AppDataBase.class, "database-name").build();
+            defaultDB = ((AppDataBase) db).defaultDao();
+            DefaultSettings defaultSettings = new DefaultSettings(1,15,false, false, false,"This is an automated message");
+            DefaultSettingsDao.createDefaultSettings(defaultSettings);
+            //fairly sure this is incorrect, ignore for now
+        }
+    }
 }
+
+
