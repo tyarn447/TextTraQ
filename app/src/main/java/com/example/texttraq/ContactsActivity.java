fd72010ec57f214261b450fcdf9b30536431fd99
tyarn447 <tyarn447@gmail.com>
2019-04-01 20:57:02 -0400

    Floating button now can add contacts to database, once I have implemented the actual adding, showing off that it works tommorrow

--- app/src/main/java/com/example/texttraq/ContactsActivity.java
+++ app/src/main/java/com/example/texttraq/ContactsActivity.java
@@ -1,12 +1,22 @@
 package com.example.texttraq;
 
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactsActivity extends AppCompatActivity {
+    public static final int PICK_CONTACT = 1;
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +37,36 @@ public class ContactsActivity extends AppCompatActivity {
     public void goToSettings(View view) {
         Intent intent = new Intent(this, settingsActivity.class);
         startActivity(intent);
+    }
+
+    public void findContact(View view){
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data){
+        super.onActivityResult(reqCode,resultCode,data);
+        ContentResolver cr = getContentResolver();
+
+        if(reqCode == PICK_CONTACT){
+            if(resultCode == Activity.RESULT_OK){
+                Uri contactData = data.getData();
+                Cursor c = cr.query(contactData,null,null,null,null);
+                if(((Cursor) c).moveToFirst()){
+                    String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                    String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                    Cursor phones = cr.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ contactId,null, null);
+                    if(phones.moveToFirst()) {
+                        String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        Button nameButton = findViewById(R.id.button);
+                        Button numberButton = findViewById(R.id.button2);
+                        nameButton.setText(name);
+                        numberButton.setText(phoneNumber);
+                    }
+                }
+            }
+        }
+
     }
 }
