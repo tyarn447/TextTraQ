c29a1b301942a8bd9173d35465a98ce2ba1e1ab9
tyarn447 <tyarn447@gmail.com>
2019-04-20 15:02:14 -0400

    Can text Contacts now on every minute that you want them to be texted

--- app/src/main/java/com/example/texttraq/AppDataBase.java
+++ app/src/main/java/com/example/texttraq/AppDataBase.java
@@ -9,26 +9,6 @@ import androidx.room.RoomDatabase;
 @Database(entities = {DefaultSettings.class, ContactTable.class, StateTable.class}, version = 2, exportSchema = false)
 public abstract class AppDataBase extends RoomDatabase {
 
-    public abstract ContactTableDao wordDao();
-    private static AppDataBase INSTANCE;
-
-    static AppDataBase getDatabase(final Context context) {
-        if (INSTANCE == null) {
-            synchronized (AppDataBase.class) {
-                if (INSTANCE == null) {
-                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDataBase.class, "word_database")
-                            // Wipes and rebuilds instead of migrating
-                            // if no Migration object.
-                            // Migration is not part of this practical.
-                            .fallbackToDestructiveMigration()
-                            .build();
-                }
-            }
-        }
-        return INSTANCE;
-    }
-
     public abstract DefaultSettingsDao getDefaultDao();
     public abstract ContactTableDao getContactDao();
     public abstract StateTableDao getStateDao();
--- app/src/main/java/com/example/texttraq/ContactsActivity.java
+++ app/src/main/java/com/example/texttraq/ContactsActivity.java
@@ -1,54 +1,23 @@
 package com.example.texttraq;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Application;
 import android.content.ContentResolver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
-//import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.room.ColumnInfo;
-import androidx.room.Dao;
-import androidx.room.Database;
-import androidx.room.Entity;
-import androidx.room.Insert;
-import androidx.room.PrimaryKey;
-import androidx.room.Query;
 import androidx.room.Room;
-import androidx.room.RoomDatabase;
-import androidx.sqlite.db.SupportSQLiteDatabase;
 
 public class ContactsActivity extends AppCompatActivity {
     public static final int PICK_CONTACT = 1;
@@ -63,24 +32,6 @@ public class ContactsActivity extends AppCompatActivity {
 
 
 
-    /* Notes For Alex From Taylor
-    First on the recycler view make sure each button shows the name and number so we have a way of getting
-    both the name and number from the button,
-    Then when you are able to click each one individual you want to add both the name and number into the intent,
-    ie. you want to add the Name of the person into the EXTRAMESSAGE i have defined above, and you want to add the
-    Number of the person into the EXTRAMESSAGE2 I have defined above as well, once those are both added into the intent
-    you pass the intent along to the contactSettings activity, I have created the contact settings activity so that it will
-    take both those extramessages you have passed along and make the page header have the persons name and so giving
-    the page the name and number is crucial because it allows me to grab their info from the database so that we can
-    initialze the page with it so it looks correct, also allows me to update their settings after when the apply button is pressed,
-    that page is essentially finished just need to get the recycler view and pressing each one working... think you need
-    to do something with an onclicklistener for this.
-
-    AppDataBase db = Room.databaseBuilder(this, AppDataBase.class, "db-data").allowMainThreadQueries().build();
-    ContactTableDao contactTableDao = db.getContactDao();
-
-
-     */
 
 
 
@@ -114,11 +65,8 @@ public class ContactsActivity extends AppCompatActivity {
                 String num = separated[1];
                 sendNameNum(name,num);
             }
+
         });
-
-
-
-
     }
 
     public void sendNameNum(String name, String num){
@@ -126,6 +74,7 @@ public class ContactsActivity extends AppCompatActivity {
         newIntent.putExtra(EXTRAMESSAGE,name);
         newIntent.putExtra(EXTRAMESSAGE2,num);
         startActivity(newIntent);
+        finish();
     }
 
     public void goToContacts(View view) {
@@ -194,5 +143,7 @@ public class ContactsActivity extends AppCompatActivity {
                 }
             }
         }
+        finish();
+        startActivity(getIntent());
     }
 }
